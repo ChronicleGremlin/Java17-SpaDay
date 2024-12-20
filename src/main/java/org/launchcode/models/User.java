f9f55df3d6a52b1bd640eb612b935b7b3297a338
Alec <alecjost7@gmail.com>
2024-12-19 20:18:48 -0600

    User information validation serverside, error message not displaying/shows whitepage

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,8 +1,25 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.Objects;
+
 public class User {
+
+    private int id;
+    private int nextId = 1;
+
+    @NotBlank(message ="Username cannot be blank")
+    @Size(min = 5, max = 15, message ="Username must be more than five characters and less than fifteen")
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message ="Password cannot be blank")
+    @Size(min = 6, message ="Password must be more than six character")
     private String password;
 
     public User() {
@@ -10,10 +27,12 @@ public class User {
     }
 
     public User(String username, String email, String password) {
-        this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.id = nextId;
+        nextId++;
+
     }
 
     public String getUsername() {
@@ -38,6 +57,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof User user)) return false;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
 
